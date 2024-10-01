bed2f43a2b490a1748b0f7f7a6ba46f1ce036d85
Jade Barry <jakebarry@outlook.com>
2024-10-01 11:35:59 -0400

    Update main.js

--- sessions/2024/main.js
+++ sessions/2024/main.js
@@ -9,12 +9,17 @@ const videoTitle = document.querySelector(".video-gallery .video-title");
 const showVideo = (videoId, title) => {
   let videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
   videoPlayer.setAttribute("src", videoUrl);
-  videoTitle.innerHTML = title;
+  
+  // Check if videoTitle exists before setting innerHTML
+  if (videoTitle) {
+    videoTitle.innerHTML = title;
+  }
 };
 
 videoThumbnails.forEach((v) => {
   v.addEventListener("click", () => {
     showVideo(v.dataset.id, v.dataset.title);
+    
     // Scroll to the top of the page smoothly
     window.scrollTo({
       top: 0,
